6ee766d6d47859f1f28c92f21e922c4ca7dd48bb
Arnab Banerjee <arnab.ban09@gmail.com>
2019-09-07 23:55:40 +0530

    Initial Commit in Spring Boot

---
+++ CoreJavaTutorial/src/main/java/com/arnab/java/tutorial/CoreJavaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.arnab.java.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJavaTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJavaTutorialApplication.class, args);
+	}
+
+}
